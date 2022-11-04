768c9216b959feab0176bde715593d6126e931cc
Devin Rhode <539816+devinrhode2@users.noreply.github.com>
2022-11-04 16:50:34 -0500

    Return wrapped analytics utils, don't depend on global window.analytics

--- src/shared-analytics/trackEvents.ts
+++ src/shared-analytics/trackEvents.ts
@@ -1,31 +1,34 @@
 import type { UserModel } from './UserModel';
 import type { ACTION, SOURCE } from './constants';
-import type { Analytics } from '@segment/analytics-next';
+import type { Analytics, AnalyticsBrowser } from '@segment/analytics-next';
 
-// copy from https://github.com/rye-com/dev-console/blob/ccdaf725d7a9782cdd1a020b7b97399e4aaa4528/src/app/utils.ts
-export const trackIdentifyEvent = (userInfoModel: UserModel) => {
-  window.analytics.identify(userInfoModel.uid, {
-    email: userInfoModel.email,
-  });
-};
+export const getAnalyticsUtils = (analytics: AnalyticsBrowser) => {
+  return {
+    identify: (userInfoModel: UserModel) => {
+      analytics.identify(userInfoModel.uid, {
+        email: userInfoModel.email,
+      });
+    },
 
-export const trackPageEvent = (pageName: string) => {
-  window.analytics.page(pageName);
-};
+    page: (pageName: string) => {
+      analytics.page(pageName);
+    },
 
-export const trackEvent = (
-  source: SOURCE,
-  action: ACTION,
-  noun: string,
-  properties?: Parameters<Analytics['track']>[1],
-  // Not used yet. Uncomment if you need these.
-  // _options?: Parameters<Analytics['track']>[2],
-  // _callback?: Parameters<Analytics['track']>[3],
-) => {
-  window.analytics.track(
-    [source.valueOf(), action.valueOf(), noun].join('_'),
-    properties,
-    // _options,
-    // _callback,
-  );
+    track: (
+      source: SOURCE,
+      action: ACTION,
+      noun: string,
+      properties?: Parameters<Analytics['track']>[1],
+      // Not used yet. Uncomment if you need these.
+      // _options?: Parameters<Analytics['track']>[2],
+      // _callback?: Parameters<Analytics['track']>[3],
+    ) => {
+      analytics.track(
+        [source.valueOf(), action.valueOf(), noun].join('_'),
+        properties,
+        // _options,
+        // _callback,
+      );
+    },
+  };
 };
