8a27852de1325082d64ebd85e230170dc57e3ff2
Devin Rhode <539816+devinrhode2@users.noreply.github.com>
2022-11-04 16:38:05 -0500

    Shorten analytics util names, cleanup optional chaining

--- src/shared-analytics/trackEvents.ts
+++ src/shared-analytics/trackEvents.ts
@@ -2,25 +2,23 @@ import type { UserModel } from './UserModel';
 import type { ACTION, SOURCE } from './constants';
 
 // copy from https://github.com/rye-com/dev-console/blob/ccdaf725d7a9782cdd1a020b7b97399e4aaa4528/src/app/utils.ts
-export const trackSegmentIdentifyEvent = (userInfoModel: UserModel) => {
-  window?.analytics?.identify(userInfoModel.uid, {
+export const trackIdentifyEvent = (userInfoModel: UserModel) => {
+  window.analytics.identify(userInfoModel.uid, {
     email: userInfoModel.email,
   });
 };
 
-export const trackSegmentPageEvent = (pageName: string) => {
-  /* @ts-ignore */
-  window?.analytics?.page(pageName);
+export const trackPageEvent = (pageName: string) => {
+  window.analytics.page(pageName);
 };
 
-export const trackSegmentEvent = (
+export const trackEvent = (
   source: SOURCE,
   action: ACTION,
   noun: string,
-  ...rest: { [key: string]: any }[]
+  ...rest: Array<{ [key: string]: any }>
 ) => {
-  /* @ts-ignore */
-  window?.analytics?.track(
+  window.analytics.track(
     [source.valueOf(), action.valueOf(), noun].join('_'),
     filterUndefinedProps({
       ...rest,
