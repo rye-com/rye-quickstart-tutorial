1a7e8f83e703a7ea788e2b343389bd94305e6e15
Devin Rhode <539816+devinrhode2@users.noreply.github.com>
2022-11-04 16:44:58 -0500

    Remove support for `any` data to include with request

--- src/shared-analytics/trackEvents.ts
+++ src/shared-analytics/trackEvents.ts
@@ -1,5 +1,6 @@
 import type { UserModel } from './UserModel';
 import type { ACTION, SOURCE } from './constants';
+import type { Analytics } from '@segment/analytics-next';
 
 // copy from https://github.com/rye-com/dev-console/blob/ccdaf725d7a9782cdd1a020b7b97399e4aaa4528/src/app/utils.ts
 export const trackIdentifyEvent = (userInfoModel: UserModel) => {
@@ -16,22 +17,10 @@ export const trackEvent = (
   source: SOURCE,
   action: ACTION,
   noun: string,
-  ...rest: Array<{ [key: string]: any }>
+  properties?: Parameters<Analytics['track']>[1],
 ) => {
   window.analytics.track(
     [source.valueOf(), action.valueOf(), noun].join('_'),
-    filterUndefinedProps({
-      ...rest,
-    }),
+    properties,
   );
-};
-
-export const filterUndefinedProps = (obj: object) => {
-  return Object.keys(obj).reduce((result, key) => {
-    const propKey = key as keyof typeof obj;
-    if (obj[propKey] !== undefined && obj[propKey] !== null) {
-      result[propKey] = obj[propKey];
-    }
-    return result;
-  }, {});
 };
