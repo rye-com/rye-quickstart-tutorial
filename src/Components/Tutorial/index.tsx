3c7b239b909b8a4c304fb1ba78291b16a7450ce3
Devin Rhode <539816+devinrhode2@users.noreply.github.com>
2022-11-07 17:30:22 -0600

    Extract step 2-requestProductData to separate file

--- src/Components/Tutorial/index.tsx
+++ src/Components/Tutorial/index.tsx
@@ -9,7 +9,6 @@ import {
   Label,
   Select,
   Spinner,
-  Tabs,
   TextInput,
   Timeline,
 } from 'flowbite-react';
@@ -51,7 +50,6 @@ import { ThemeEnum, MarketplaceEnum } from './types';
 import { useDebouncedEffect } from '../../hooks/useDebouncedEffect';
 import { getDefaultStore } from '../../localStorage-crud/getDefaultStore';
 import { CustomTimelineBody } from './helper-components/CustomTimelineBody';
-import { InlineCodeSnippet } from './helper-components/InlineCodeSnippet';
 import { CustomCodeBlock } from './helper-components/CustomCodeBlock';
 import { RequestResponseCodeBlock } from './helper-components/ResponseCodeBlock';
 import type { StripeProp } from './types/StripeProp';
@@ -59,6 +57,7 @@ import type { Ryelytics } from '../../shared-analytics/getRyelytics';
 import { ACTION, SOURCE } from '../../shared-analytics/constants';
 import { enterApiKey } from './tutorial-steps/0-enterApiKey';
 import { requestScrape } from './tutorial-steps/1-requestScrape';
+import { requestProductData } from './tutorial-steps/2-requestProductData';
 
 const defaultStore = getDefaultStore();
 
@@ -585,113 +584,19 @@ export default function Index({ ryelytics }: { ryelytics: Ryelytics }) {
               requestedProductSnippetLineNumber,
               requestedProductSnippet,
             )}
-            <Timeline.Item>
-              <Timeline.Content>
-                <div className="flex">
-                  <Card className="max-w-xl self-baseline">
-                    <Timeline.Title>Fetch an item from the Rye API</Timeline.Title>
-                    <CustomTimelineBody>
-                      <Timeline.Point />
-                      <div className="py-1">
-                        Once an item is requested, it can be retrieved using the Rye API
-                      </div>
-                    </CustomTimelineBody>
-                    <CustomTimelineBody>
-                      <Timeline.Point />
-                      <Tabs.Group
-                        aria-label="Full width tabs"
-                        // eslint-disable-next-line react/style-prop-object
-                        style="underline"
-                        onClick={onMarketplaceChange}
-                      >
-                        <Tabs.Item
-                          title="Amazon"
-                          active={
-                            data.requestedProduct.selectedMarketplace === MarketplaceEnum.Amazon
-                          }
-                        >
-                          <div className="mt-3">
-                            The Amazon Standard identification Number (ASIN) can be used to fetch an
-                            item from Amazon. You can find the item ID for an amazon item by using
-                            the ID after <InlineCodeSnippet>dp/</InlineCodeSnippet> in the URL.
-                            Example:
-                          </div>
-                          <span className="text-amber-700">
-                            https://amazon.com/neo-sporin/dp/
-                            <span className="rounded-lg bg-red-500 px-2 text-amber-300">
-                              B000NQ10FK
-                            </span>
-                          </span>
-                          <div className="mt-3">
-                            It is also returned in the response by the{' '}
-                            <InlineCodeSnippet>requestProduct</InlineCodeSnippet> mutation.
-                          </div>
-                        </Tabs.Item>
-                        <Tabs.Item
-                          title="Shopify"
-                          active={
-                            data.requestedProduct.selectedMarketplace === MarketplaceEnum.Shopify
-                          }
-                        >
-                          <div className="py-3">
-                            The Shopify product ID can be found in the response of the{' '}
-                            <InlineCodeSnippet>requestProduct</InlineCodeSnippet> mutation.
-                          </div>
-                        </Tabs.Item>
-                      </Tabs.Group>
-                    </CustomTimelineBody>
-                    <CustomTimelineBody>
-                      <div>
-                        <Label htmlFor="marketplace_request" value="Marketplace" />
-                        <TextInput
-                          disabled
-                          className="my-3 w-full"
-                          id="marketplace_request"
-                          value={data.requestedProduct.selectedMarketplace}
-                        ></TextInput>
-                        <Timeline.Point />
-                        <Label
-                          htmlFor="fetch_product_id"
-                          value="Enter product ID"
-                          className="mt-10"
-                        />
-                        <div className="my-3 flex">
-                          <TextInput
-                            value={selectedProductID || ''}
-                            // @ts-expect-error - TS2322 - "color props are not compatible with exactOptionalPropertyTypes: true" - shouldn't really matter, as long as js is checking for truthiness
-                            icon={BarcodeIcon}
-                            className="w-full"
-                            id="fetch_product_id"
-                            placeholder={marketPlaceSelector('2863039381604', 'B000NQ10FK')}
-                            onChange={onProductIDChange}
-                          ></TextInput>
-                          <Button
-                            style={{ width: 150, height: 40, maxHeight: 40 }}
-                            onClick={fetchProduct}
-                            className="mx-3"
-                            disabled={isFetchingProduct}
-                          >
-                            {!isFetchingProduct ? 'Fetch' : <Spinner style={{ maxHeight: 30 }} />}
-                          </Button>
-                        </div>
-                        <RequestResponseCodeBlock
-                          response={fetchProductResponse}
-                          currentTheme={currentTheme}
-                        />
-                      </div>
-                    </CustomTimelineBody>
-                  </Card>
-                  <div className="mx-3 max-w-xl overflow-scroll">
-                    <CustomCodeBlock
-                      showLineNumbers={true}
-                      startingLineNumber={productFetchSnippetLineNumber}
-                      currentTheme={currentTheme}
-                      codeString={productFetchSnippet}
-                    ></CustomCodeBlock>
-                  </div>
-                </div>
-              </Timeline.Content>
-            </Timeline.Item>
+            {requestProductData(
+              onMarketplaceChange,
+              data,
+              selectedProductID,
+              marketPlaceSelector,
+              onProductIDChange,
+              fetchProduct,
+              isFetchingProduct,
+              fetchProductResponse,
+              currentTheme,
+              productFetchSnippetLineNumber,
+              productFetchSnippet,
+            )}
             <Timeline.Item>
               <Timeline.Content>
                 <div className="flex">
