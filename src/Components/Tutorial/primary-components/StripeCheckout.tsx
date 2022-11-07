2d01da44eaa80825fd8cbe9cf8fb5e8989797978
Devin Rhode <539816+devinrhode2@users.noreply.github.com>
2022-11-06 22:10:11 -0600

    Revamp checkout overlay
    +tweaked `top-36` to `top-24`

--- src/Components/Tutorial/primary-components/StripeCheckout.tsx
+++ src/Components/Tutorial/primary-components/StripeCheckout.tsx
@@ -2,6 +2,7 @@ import { Elements as StripeElements } from '@stripe/react-stripe-js';
 import { CheckoutForm } from '../../CheckoutForm';
 import { ThemeEnum } from '../types';
 import type { StripeProp } from '../types/StripeProp';
+import { emailTo, emailToTestCheckoutHref } from './email-subject-and-body';
 
 export const StripeCheckout = ({
   stripePromise,
@@ -36,16 +37,43 @@ export const StripeCheckout = ({
     return StripeForm;
   } else {
     return (
-      <div
-        style={{
-          pointerEvents: 'none',
-          cursor: 'not-allowed',
-          opacity: 0.5,
-          backgroundColor: 'black',
-        }}
-      >
-        <h2 className="text-1xl my-2">Email support@rye.com to test checkout</h2>
-        {StripeForm}
+      <div>
+        <div
+          style={{
+            // width is set to cause line breaks like so:
+            //   Email dev@rye.com
+            //   to test checkout
+            width: '345px',
+            textAlign: 'center',
+            margin: 'auto',
+          }}
+        >
+          <h1
+            className="relative top-24 z-10 text-center text-4xl font-bold text-black"
+            style={{
+              textShadow: [
+                [10, 10, 10, 10, 10, 10, 10, 10].map((i) => `0px 0px ${i}px #fff`).join(', '),
+                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => `0px 0px ${i * 2}px #eee`).join(', '),
+                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => `0px 0px ${i * 3}px #ddd`).join(', '),
+              ].join(', '),
+              height: '0px',
+            }}
+          >
+            Email
+            {/* prettier-ignore */}
+            <a href={emailToTestCheckoutHref} target="_blank" rel="noopener noreferrer">
+              {/* Extra space slightly expands clickable area: */}
+              {' '}
+              {/* underline looks weird with text-shadows on h1 */}
+              <span className="text-blue-500">{emailTo}</span>
+              {' '}
+            </a>
+            to test checkout
+          </h1>
+        </div>
+        <div className="cursor-not-allowed transition-opacity hover:opacity-50">
+          <div className="pointer-events-none">{StripeForm}</div>
+        </div>
       </div>
     );
   }
