e096b9608dc0872eec00cce7bfe2573dc4c05feb
Devin Rhode <539816+devinrhode2@users.noreply.github.com>
2022-11-11 15:47:54 -0500

    Pass single object to .track

--- src/shared-analytics/getRyelytics.ts
+++ src/shared-analytics/getRyelytics.ts
@@ -1,7 +1,31 @@
 import type { UserModel } from './UserModel';
 import type { ACTION, SOURCE } from './constants';
-import type { Analytics } from '@segment/analytics-next';
 import { AnalyticsBrowser } from '@segment/analytics-next';
+
+type RyelyticsProperties = {
+  /** Feel free to add new SOURCE values to shared-analytics/constants */
+  source: SOURCE;
+  /** Feel free to add new ACTION values to shared-analytics/constants */
+  action: ACTION;
+  /**
+   * A proper noun, like "Request scrape button", not "Request scrape"
+   *
+   * noun is used to generate the eventName like so:
+   * ```tsx
+   * const eventName = capitalize(noun.replaceAll('_', ' '))
+   * ```
+   */
+  noun: string;
+
+  /** User inputs or request parameters */
+  params?: Record<string, string | boolean | number>;
+
+  // In google BigQuery (segment integration) nested data gets flattened out: { params: { id } } becomes { params_id }.
+  // Therefore, we want to avoid nesting in a way that is "too" noisy.
+  // Some nesting makes reading/writing code easier.
+  // We also want to avoid prop spreading: `...props`.
+  // Therefore, feel free to add new keys for new categories of data.
+};
 
 // Based on: https://github.com/rye-com/dev-console/blob/ccdaf725d7a9782cdd1a020b7b97399e4aaa4528/src/app/utils.ts#L59
 export const getRyelytics = () => {
@@ -34,17 +58,16 @@ export const getRyelytics = () => {
       return analytics.page(pageName);
     },
 
-    track: (
-      source: SOURCE,
-      action: ACTION,
-      noun: string,
-      properties?: Parameters<Analytics['track']>[1],
-    ) => {
-      const eventName = [source, action, noun].join('_');
+    track: (event: RyelyticsProperties) => {
       if (enableLogging) {
-        console.log('ryelytics.track', eventName, properties);
+        console.log('ryelytics.track', event);
       }
-      return analytics.track(eventName, properties);
+      return analytics.track(
+        // Searching google for "segment analytics.track", opening first 10 results, and searching those pages for ".track(" reveals a clear pattern:
+        // They always pass an eventName initial string like "Clicked Pay" before the event properties.
+        [event.source, event.action, event.noun].join('_'),
+        event,
+      );
     },
   };
 };
