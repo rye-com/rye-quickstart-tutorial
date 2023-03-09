af88212cfbf3daabc4af4f9ff0145b9c95afb31f
richard@rye.com <richard@rye.com>
2023-03-09 11:52:08 -0800

    rebase fix

--- src/Components/Tutorial/index-new.tsx
+++ src/Components/Tutorial/index-new.tsx
@@ -4,16 +4,12 @@ import TutorialNav from './tutorial-nav';
 import TutorialContent from './tutorial-content';
 import type { TutorialStep } from './types';
 import { TUTORIAL_STEPS } from './constants';
-<<<<<<< HEAD
 import type { NonEmptyArray } from './constants';
-=======
->>>>>>> 0700896 (Add start enhancher feature for external links)
 
 export type TutorialProps = {
   compact?: boolean;
 };
 
-<<<<<<< HEAD
 type UrlMapType = {
   [url: string]: TutorialStep;
 };
@@ -31,12 +27,6 @@ export default function Index(props: TutorialProps) {
   const urlMap = createUrlToTutorialMap(TUTORIAL_STEPS);
   const step = urlMap[window.location.pathname] || urlMap['/start'] || TUTORIAL_STEPS[0];
   const [currentStep, setCurrentStep] = useState<TutorialStep>(step);
-=======
-export default function Index(props: TutorialProps) {
-  console.log(props);
-  const [currentStep, setCurrentStep] = useState<TutorialStep>(TUTORIAL_STEPS[0]);
-
->>>>>>> 0700896 (Add start enhancher feature for external links)
   return (
     <div className="grid grid-cols-4">
       <TutorialNav currentStep={currentStep} setCurrentStep={setCurrentStep} />
