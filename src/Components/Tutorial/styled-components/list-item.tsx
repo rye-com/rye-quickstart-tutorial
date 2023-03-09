760d3daa3c6063f5f8b3e93ea983656df1247f2d
richard@rye.com <richard@rye.com>
2023-03-09 15:38:33 -0800

    use nullish coelescing

--- src/Components/Tutorial/styled-components/list-item.tsx
+++ src/Components/Tutorial/styled-components/list-item.tsx
@@ -11,8 +11,8 @@ type Props = {
 
 export default function ListItem({ children, content, styleOverrides }: Props) {
   const { list, paragraph } = styleOverrides || {};
-  const listStyles = list ? list : 'mb-[28px]';
-  const paragraphStyles = paragraph ? paragraph : 'mb-[12px] inline';
+  const listStyles = list ?? 'mb-[28px]';
+  const paragraphStyles = paragraph ?? 'mb-[12px] inline';
 
   return (
     <li className={listStyles}>
