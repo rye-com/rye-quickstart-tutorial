fc4a8f12427dca8dcacf84d5470544ee22c294ae
richard@rye.com <richard@rye.com>
2023-03-08 17:03:41 -0800

    Add static content for getting started

---
+++ src/Components/Tutorial/styled-components/list-item.tsx
@@ -0,0 +1,23 @@
+import type { ReactNode } from 'react';
+
+type Props = {
+  children?: ReactNode;
+  content?: string;
+  styleOverrides?: {
+    list?: string;
+    paragraph?: string;
+  };
+};
+
+export default function ListItem({ children, content, styleOverrides }: Props) {
+  const { list, paragraph } = styleOverrides || {};
+  const listStyles = list ? list : 'mb-[28px]';
+  const paragraphStyles = paragraph ? paragraph : 'mb-[12px] inline';
+
+  return (
+    <li className={listStyles}>
+      <p className={paragraphStyles}>{content}</p>
+      {children}
+    </li>
+  );
+}
--- src/Components/Tutorial/tutorial-content/getting-started.tsx
+++ src/Components/Tutorial/tutorial-content/getting-started.tsx
@@ -1,7 +1,22 @@
+import RyeApiKeyV2 from '../../../assets/rye-key-v2.png';
+import ListItem from '../styled-components/list-item';
+
 export default function GettingStarted() {
   return (
     <ol className="list-inside list-decimal text-paragraph-medium font-semibold">
-      <li>CONTENT HERE</li>
+      <ListItem content="Sign up and log in to" />
+      <ListItem
+        styleOverrides={{ paragraph: 'inline-block mb-[12px]' }}
+        content="Under Account → Access and Security, view and copy your API key"
+      >
+        <img src={RyeApiKeyV2} alt="API Key" />
+      </ListItem>
+      <ListItem content="Enter your Rye API key" />
+      <ListItem content="Now you can use your API key to access the Rye API throughout the tutorial">
+        <p className="mb-[12px] text-paragraph-small font-normal">
+          See below for the code snippet to call the Rye API:
+        </p>
+      </ListItem>
     </ol>
   );
 }
