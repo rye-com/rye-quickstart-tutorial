69eca273f62566427f203381036186468f857093
richard@rye.com <richard@rye.com>
2023-03-13 18:07:20 -0700

    Move ExternalLink class map to module scope

--- src/Components/Tutorial/styled-components/external-link.tsx
+++ src/Components/Tutorial/styled-components/external-link.tsx
@@ -1,6 +1,5 @@
 import { LinkType } from '../constants';
 import type { ComponentProps } from 'react';
-import { useMemo } from 'react';
 
 type Props = {
   href: string;
@@ -9,14 +8,13 @@ type Props = {
   startEnhancer?: React.FC<ComponentProps<'svg'>>;
 };
 
+const linkClass = {
+  [LinkType.Pill]: 'rounded-2xl bg-brand-green pr-[12px] pl-[12px] pt-[8px] pb-[8px]',
+};
+
 export default function ExternalLink(props: Props) {
   const { href, text, type, startEnhancer: StartEnhancer } = props;
-  const linkClass = useMemo(
-    () => ({
-      [LinkType.Pill]: 'rounded-2xl bg-brand-green pr-[12px] pl-[12px] pt-[8px] pb-[8px]',
-    }),
-    [],
-  );
+
   return (
     <a target="_blank" className={linkClass[type]} href={href} rel="noreferrer">
       {StartEnhancer ? <StartEnhancer className="mb-[4px] mr-[4px] inline h-4 w-4" /> : null}
