30da9266e2b8f2b7aec45bb279b302f9d33bf358
richard@rye.com <richard@rye.com>
2023-03-14 15:35:16 -0700

    move urlMap outside of scope and remove comments

--- src/Components/Tutorial/index-new.tsx
+++ src/Components/Tutorial/index-new.tsx
@@ -1,11 +1,9 @@
 import TutorialNav from './tutorial-nav';
-// import TutorialContent from './tutorial-content';
 import type { TutorialStep } from './types';
 import { TUTORIAL_STEPS } from './constants';
 import type { NonEmptyArray } from './constants';
 import { Outlet } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
-import { useMemo } from 'react';
 
 type UrlMapType = {
   [url: string]: TutorialStep;
@@ -19,9 +17,10 @@ function createUrlToTutorialMap(steps: NonEmptyArray<TutorialStep>): UrlMapType 
   return urlMap;
 }
 
+const urlMap = createUrlToTutorialMap(TUTORIAL_STEPS);
+
 export default function Index() {
   const location = useLocation();
-  const urlMap = useMemo(() => createUrlToTutorialMap(TUTORIAL_STEPS), []);
   const step = urlMap[location.pathname] || urlMap['/start'] || TUTORIAL_STEPS[0];
   return (
     <div className="grid grid-cols-4">
