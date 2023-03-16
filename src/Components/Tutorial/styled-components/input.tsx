7d852acf537f6f22989e012b9659031ed63d985c
richard@rye.com <richard@rye.com>
2023-03-16 08:55:15 -0700

    use focus within instead of state

--- src/Components/Tutorial/styled-components/input.tsx
+++ src/Components/Tutorial/styled-components/input.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import classNames from 'classnames';
 import type { ComponentProps } from 'react';
 
@@ -10,15 +10,12 @@ type Props = {
 
 //TODO(RYE-1994): implement accessibility props
 export default function Input(props: Props) {
-  const [isFocused, setIsFocused] = useState(false);
   const { startEnhancer: StartEnhancer, onChange, placeholder } = props;
   return (
     <div
       className={classNames(
-        'flex w-full overflow-hidden rounded-2xl border-2 border-solid bg-white text-paragraph-medium font-normal',
+        'group flex w-full overflow-hidden rounded-2xl border-2 border-solid bg-white text-paragraph-medium font-normal focus-within:border-black',
         {
-          'border-action-light-grey': !isFocused,
-          'border-black': isFocused,
           'pl-[12px]': !!StartEnhancer,
         },
       )}
@@ -30,12 +27,6 @@ export default function Input(props: Props) {
       ) : null}
 
       <input
-        onFocus={() => {
-          setIsFocused(true);
-        }}
-        onBlur={() => {
-          setIsFocused(false);
-        }}
         onChange={onChange}
         placeholder={placeholder}
         className="w-full max-w-full pl-[10px] pr-[10px] pt-[8px] pb-[8px] focus:outline-none"
