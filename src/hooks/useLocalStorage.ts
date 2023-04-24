4449494a93a90a009bd7ee07dcff8b72b721a773
David Choi <davidchoi@Davids-MacBook-Air.local>
2023-04-24 13:18:54 -0700

    [Feat] Add API Key to localStorage

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+type SetValue<T> = (value: T | ((val: T) => T)) => void;
+
+function useLocalStorage<T>(key: string, initialValue: T): [T, SetValue<T>] {
+  const [value, setValue] = useState<T>(() => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue !== null ? JSON.parse(storedValue) : initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
+
+export default useLocalStorage;
