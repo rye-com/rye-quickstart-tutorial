3c396fd6c96aedffb1a7f1e5aaa3709df84c6af3
Saurabh S <foklepoint@users.noreply.github.com>
2023-05-11 16:35:58 -0700

    Delete all unused code (#71)

--- src/shared-analytics/snakeCaseKeys.ts
+++
@@ -1,5 +0,0 @@
-import mapKeys from 'lodash/mapKeys';
-import snakeCase from 'lodash/snakeCase';
-
-export const snakeCaseKeys = <T extends Record<string, unknown>>(obj: T) =>
-  mapKeys(obj, (_value, key) => snakeCase(key));
