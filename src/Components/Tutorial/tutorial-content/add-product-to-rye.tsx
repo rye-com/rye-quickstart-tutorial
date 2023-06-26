2affab1165dc32516682970196d1ea8ecd00b559
Akshay Saxena <41586370+akshay2000saxena@users.noreply.github.com>
2023-06-26 10:31:19 -0400

    [Feat] Add support for Shopify products for "Add product to Rye" (#73)
    * Add support for Shopify products
    
    * Default to shopify

--- src/Components/Tutorial/tutorial-content/add-product-to-rye.tsx
+++ src/Components/Tutorial/tutorial-content/add-product-to-rye.tsx
@@ -9,9 +9,10 @@ import ListItem from '../styled-components/list-item';
 import TerminalTab from '../styled-components/code-terminal-tab';
 import Terminal from '../styled-components/code-terminal';
 
-// TODO: add support for Shopify products
 const amazonProduct = getNextRandomProduct(MarketplaceEnum.Amazon);
+const shopifyProduct = getNextRandomProduct(MarketplaceEnum.Shopify);
 const requestProductAmazonCodeSnippet = requestProductSnippet(amazonProduct.productURL, amazonProduct.selectedMarketplace);
+const requestProductShopifyCodeSnippet = requestProductSnippet(shopifyProduct.productURL, shopifyProduct.selectedMarketplace);
 
 export default function AddProductToRye() {
   const context = useContext(TutorialContext);
@@ -26,6 +27,8 @@ export default function AddProductToRye() {
     apiKey: { currentApiKey },
   } = context;
   const [fetchError, setFetchError] = useState(false);
+  const [selectedMarketplace, setSelectedMarketplace] = useState<MarketplaceEnum>(MarketplaceEnum.Shopify);
+  const selectedProductUrl = selectedMarketplace === MarketplaceEnum.Amazon ? amazonProduct.productURL : shopifyProduct.productURL;
   const requestProductDataOutputJSON = JSON.stringify(requestProductData, null, 2);
 
   const onClickFetch = () => {
@@ -50,12 +53,12 @@ export default function AddProductToRye() {
           <ListItem content="Copy the URL from an Amazon or Shopify product page">
             <div className="flex w-3/4 items-center mt-[4px]">
               <Input
-                  value={amazonProduct.productURL}
+                  value={selectedProductUrl}
                   internalLabel="Sample product URL"
               />
               <button
                   onClick={() => {
-                    navigator.clipboard.writeText(amazonProduct.productURL);
+                    navigator.clipboard.writeText(selectedProductUrl);
                   }}
                   className="mx-3 rounded-xl bg-brand-green py-[14px] px-[24px] hover:bg-brand-hover-green active:bg-brand-active-green"
               >
@@ -70,8 +73,9 @@ export default function AddProductToRye() {
               You can use the product ID returned by this mutation to fetch product data.
               You can also do this via the Rye Console.
             </p>
-            <Terminal>
-              <TerminalTab label="amazon.js" code={requestProductAmazonCodeSnippet} />
+            <Terminal onTabChange={(tabIndex) => tabIndex === 0 ? setSelectedMarketplace(MarketplaceEnum.Amazon) : setSelectedMarketplace(MarketplaceEnum.Shopify)}>
+              <TerminalTab label="amazon.js" code={requestProductAmazonCodeSnippet} selected={selectedMarketplace === MarketplaceEnum.Amazon}/>
+              <TerminalTab label="shopify.js" code={requestProductShopifyCodeSnippet} selected={selectedMarketplace === MarketplaceEnum.Shopify}/>
             </Terminal>
           </ListItem>
         </ol>
